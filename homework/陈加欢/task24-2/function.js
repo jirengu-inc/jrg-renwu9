fc4ccb7c35453aaf89273783f22eb9453e61b3c2
candy252324 <1964646121@qq.com>
2016-12-05 15:57:08 +0800

    task24

--- homework/陈加欢/task24-2/function.js
+++ homework/陈加欢/task24-2/function.js
@@ -39,46 +39,30 @@ function isValidUsername(str){
     }
 }
 
-function ajax(opts) {
-    //  做参数兼容
-    opts.success = opts.success || function(){};
-    opts.error = opts.error || function(){};
-    opts.type = opts.type || 'get';
-    opts.dataType = opts.dataType || 'json';
-    opts.data = opts.data || {};
-
-    var dataStr = '';
-    for (var key in opts.data) {
-        dataStr += key + '=' + opts.data[key] + '&';
-    }
-    dataStr = dataStr.substr(0, dataStr.length - 1);
-
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState === 4) {
-            if(xmlhttp.status === 200){
-                if(opts.dataType === 'text'){
-                    opts.success(xmlhttp.responseText);
-                }
-                if(opts.dataType === 'json'){
-                    var json = JSON.parse(xmlhttp.responseText);
-                    opts.success(json);                 
-                }
-        
-            }else{
-                opts.error();   
+ function ajax(opts){
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange=function(){
+            if(xmlhttp.readyState==4&&xmlhttp.status==200){
+                var json = JSON.parse(xmlhttp.responseText);
+                opts.success(json);
             }
-
+            if(xmlhttp.status==404){
+                opts.error();
+            }
+        };
+        var dataStr = '';
+        for(var key in opts.data){
+            dataStr += key + '=' + opts.data[key]+'&';
         }
-    };
+        dataStr = dataStr.substr(0,dataStr.length-1);
 
-    if (opts.type.toLowerCase() === 'post') {
-        xmlhttp.open(opts.type, opts.url, true);
-        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlhttp.send(dataStr);
+        if(opts.type.toLowerCase()==='get'){
+            xmlhttp.open('get',opts.url+'?'+dataStr,false);
+            xmlhttp.send();
+        }
+        if(opts.type.toLowerCase()==='post'){
+            xmlhttp.open('post',opts.url,true);
+            xmlhttp.sendRequestHeader('content-type','application/x-www-form-urlencoded');
+            xmlhttp.send(dataStr);
+        }
     }
-    if (opts.type.toLowerCase() === 'get') {
-        xmlhttp.open(opts.type, opts.url + '?' + dataStr, true);
-        xmlhttp.send();
-    }
-}
